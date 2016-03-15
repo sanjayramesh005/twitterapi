909245febeef1fd0328407c3bfe85b149c994edc
Sanjay Ramesh <sanjayramesh005@gmail.com>
2016-03-16 00:36:21 +0530

    Added comments.

--- twitter.py
+++ twitter.py
@@ -1,3 +1,5 @@
+# A python2 script for processing twitter data
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -12,6 +14,9 @@ import time
 in_size = raw_input("Input the cache size: ")
 cache_size=int(in_size)
 cache = {}
+# cache is a dictionary. The keys are the words and the values are lists with the count and the timestamp of the word.
+# eg : {'India':[1,3812481246.2487]}
+
 key = raw_input("Input the keyword: ")
 
 access_token = raw_input("Enter your access token: ")
@@ -26,9 +31,8 @@ class listener(StreamListener):
         self.tweet = ''
         self.tweet_text = ''
         
-    def on_data(self, data):
-        #print data
-        self.tweet = json.loads(data)
+    def on_data(self, data):  # This increments the count of the words as and when they are encountered.
+        self.tweet = json.loads(data) 
         if('text' in self.tweet):
             self.tweet_text = self.tweet['text']
             self.li = self.tweet_text.split()
@@ -44,39 +48,43 @@ class listener(StreamListener):
                             del cache[j]
                             cache[i] = [1,time.time()]
                             break
-        #print cache
-        #print len(cache)
         return True
 
     def on_error(self, status):
         print status
 
 
-def scorer():
+# This is a function that does the scoring after every thirty seconds.
+# This function runs in parallel with the stream in a different thread.
+
+def scorer():  
     count = 0
     while(True):
         lasttime = time.time()
         for i in cache:
-            if(lasttime-cache[i][1]>60):
+            if(lasttime-cache[i][1]>5):
                 cache[i][0]=cache[i][0]-1
 
         for i, v in cache.items():
             if v[0]<0:
                 del cache[i]
                 
-        if(count%2==0):
+        if(count==2):
             print "Contents of cache are:"
             for i in cache:
                  print i,'\t', cache[i][0]
             count=1
         else:
-            count=0    
-        time.sleep(30)
+            count=2
+
+        # the thread waits for 30 seconds before starting again
+        # some time has been subtracted below. It is the time taken for scoring the cache.
+        if (30-(time.time()-lasttime)>0):
+            time.sleep(30-(time.time()-lasttime)) 
         
 if __name__=='__main__':
-    
 
-    
+    # The stream runs in the main thread. The scorer runs in a separate thread.
     access_token = "705794186281242625-BHrd1YCM33bUCSsOMQRZpZ4BIeIGP8b"
     access_token_secret = "lS5XKMxupo9Ml2bYiCborhh76sobS1WjmtqMQYNV71QC5"
     consumer_key = "4Hw7IsUQLoGRvfhZPCOO9bguB"
@@ -86,9 +94,12 @@ if __name__=='__main__':
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token,access_token_secret)
     
-    t1 = threading.Thread(target=scorer)
+    t1 = threading.Thread(target=scorer) # This is the thread running the scorer
+    t1.daemon = True 
     t1.start()
     
+    # The following is for handling the IncompleteRead error. I am not sure if it works.
+    # This also takes care of the keyboard interrupt.
     while True:
         try:
             stream = Stream(auth, l)
@@ -98,6 +109,5 @@ if __name__=='__main__':
             continue
         except KeyboardInterrupt:
             print "Done. Ending program."
-            t1.join()
             stream.disconnect()
             break
